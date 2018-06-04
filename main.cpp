c0cacacb4c2d40852bfbf4841c8ae9252a55f078
MarekMatusiak <marek.matusiak1966@gmail.com>
2018-06-04 17:06:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 //======================================================
-// MRsieve - prime generator boolean
+// MMsieve - prime generator boolean
 
-// Copyright [2017] [mgr inz. Marek Matusiak, mgr Pawel Rzechonek]
+// Copyright [2017] [mgr inz. Marek Matusiak]
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
